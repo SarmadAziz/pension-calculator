7100c8212c36f684cd7d8c39142e8995b61ad48d
sarmadaziz <sarmad.aziz@hotmail.nl>
2024-10-05 16:05:49 +0200

    added basic swagger documentation

--- case-developer-java/src/main/java/com/befrank/casedeveloperjava/pension/controller/ParticipantDetailsController.java
+++ case-developer-java/src/main/java/com/befrank/casedeveloperjava/pension/controller/ParticipantDetailsController.java
@@ -1,6 +1,11 @@
 package com.befrank.casedeveloperjava.pension.controller;
 
 import com.befrank.casedeveloperjava.pension.service.ParticipantService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +20,13 @@ public class ParticipantDetailsController {
         this.participantService = participantService;
     }
 
+    @Operation(summary = "Get participant details by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "found participant",
+                    content = { @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = ParticipantDetailsResponse.class)) }),
+            @ApiResponse(responseCode = "404", description = "participnat not found", content = @Content)
+    })
     @GetMapping("/{participantId}")
     public ParticipantDetailsResponse getParticipantDetails(@PathVariable("participantId") Long participantId) {
         log.info("fetching participant: ", participantId);
